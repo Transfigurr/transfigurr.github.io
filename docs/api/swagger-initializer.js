8fa982f96e6540beba545518f620749d31f53fe0
alexmichaelkeith <alex.michael.keith@gmail.com>
2024-02-27 16:50:03 -0500

    update swagger url

--- docs/api/swagger-initializer.js
+++ docs/api/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://raw.githubusercontent.com/Transfigurr/Transfigurr/develop-1.1/src/Transfigurr.API.V1/openapi.json",
+    url: "https://raw.githubusercontent.com/Transfigurr/Transfigurr/main/src/Transfigurr.API.V1/openapi.json",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
